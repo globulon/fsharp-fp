dd4e81f6d81e150a91c0561db7458787057e4d50
Marc-Daniel Ortega <globulon@gmail.com>
2014-03-31 22:52:35 +0200

    [FEAT] chap1

---
+++ chap1/Hello.fs
@@ -0,0 +1,7 @@
+open System;;
+
+[<EntryPoint>]
+let main (args: string[]) = 
+    printf "Hello %s \n" args.[0]
+    0;;
+
---
+++ chap1/exs.fs
@@ -0,0 +1,21 @@
+module FP.Chap1
+
+open System;;
+
+let plusFour n = n + 4;;
+
+let sqr x: float = x * x;;
+
+let norm x y = System.Math.Sqrt ( sqr x + sqr y);;
+
+let dec n = n-1;;
+
+let rec sumInt = function 
+    | 1 -> 1
+    | n -> n + sumInt (dec n);;
+    
+let rec fibo = function
+    | 0 -> 1
+    | 1 -> 1
+    | n -> (fibo (n-1)) + (fibo (n-2));;
+    
