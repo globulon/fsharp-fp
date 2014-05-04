2ebc4ca473e4e97041d79fbc4399466afd4469b1
Marc-Daniel Ortega <globulon@gmail.com>
2014-05-04 13:36:20 +0200

    chap6 practice;;

---
+++ chap6/practice.fs
@@ -0,0 +1,68 @@
+module Chap6.Practice
+
+open System;;
+
+(* Chinese Cubes *)
+type Colour = Red | Blue | Green | Yellow | Purple;;
+type Cbox = Nothing | Cube of float * Colour * Cbox;;
+
+let cb1 = Cube(0.5, Red, Nothing);;
+let cb2 = Cube(1.0, Green, cb1);;
+let cb3 = Cube(2.0, Yellow, cb2);;
+
+let rec count = function
+  | Nothing -> 0
+  | Cube(_,_, cb) -> 1 + count cb;;
+  
+let rec insert = function
+  | (s, _, _) when s <= 0.0                    -> failwith "Invalid box size"
+  | (s, c, Nothing)                            -> Cube(s, c, Nothing)
+  | (s, c, Cube(s', _, _)) when s=s'           -> failwith "Found cube with same size"
+  | (s, c, (Cube(s', _, _) as cb)) when s > s' -> Cube(s, c, cb)
+  | (s, c, Cube(s', c', cb')) when s < s' -> Cube(s', c', insert (s, c, cb'));;
+  
+(*Differentiation*)
+type Fexpr = | Const of float
+             | Var of String
+             | Add of Fexpr * Fexpr
+             | Sub of Fexpr * Fexpr
+             | Mul of Fexpr * Fexpr
+             | Div of Fexpr * Fexpr
+             | Sin of Fexpr
+             | Cos of Fexpr
+             | Log of Fexpr
+             | Exp of Fexpr;;
+             
+let rec D = function 
+  | Const _   -> Const 0.0
+  | Var _     -> Const 1.0
+  | Add(l, r) -> Add(D l, D r)
+  | Sub(l, r) -> Sub(D l, D r)
+  | Mul(l, r) -> Add(Mul(D l, r), Mul(l, D r))
+  | Div(l, r) -> Div(Sub(Mul(D l, r), Mul(l, D r)), Mul(r, r))
+  | Sin f     -> Mul(Cos f, D f)
+  | Cos f     -> Mul(Const -1.0, Mul(Cos f, D f))
+  | Log f     -> Div(D f, f)
+  | (Exp f)   -> Mul(D f, Exp f);;
+  
+(*Binary trees*)
+type BinTree<'a,'b> = | Leaf of 'a
+                      | Node of BinTree<'a,'b> * 'b * BinTree<'a,'b>;;
+          
+let t1 = Node(Node(Leaf 1, "cd" , Leaf 2), "ab", Leaf 3);;
+
+let rec depth = function
+  | Leaf _         -> 0
+  | Node (l, _, r) -> 1 + depth l + depth  r;;
+                      
+let rec preOrder  = function 
+  | (Leaf _,  f)       -> []
+  | (Node(l, v, r), f) -> (f v)::preOrder (l,f) @ preOrder(r, f);; 
+  
+let rec inOrder  = function 
+  | (Leaf _,  f)       -> []
+  | (Node(l, v, r), f) -> inOrder (l,f) @ [(f v)] @ inOrder (r, f);; 
+  
+let rec postOrder  = function 
+  | (Leaf _,  f)       -> []
+  | (Node(l, v, r), f) -> postOrder(l,f) @  postOrder(r, f) @ [(f v)];; 
