b69ac710ad6c07847ade83bbee3a079e3b69e6b2
Marc-Daniel Ortega <globulon@gmail.com>
2014-05-18 15:47:56 +0200

    chap7 learning

--- chap7/Curve.fs
+++ chap7/Curve.fs
@@ -11,7 +11,29 @@ let mapTail f (C(p0, ps)) = C(p0, List.map f ps)
 type Curve with
   static member ( + ) (c1, c2) = 
     match (c1, c2) with (C(h1, ps1), C(h2, ps2)) -> C(h1, ps1@(h2::ps2))
-  static member ( * ) (k, (C((x0, y0), _) as c)) = 
+  static member ( * ) ((C((x0, y0), _) as c), k) = 
     let multK (x,y) = (x0 + k*(x - x0), y0 + k*(y-y0)) 
-    mapTail multK c   
+    mapTail multK c  
+  static member ( |^) ((C((x0, y0), _) as c), theta: float) =
+    let cs = cos (theta * System.Math.PI / 180.0)
+    let sn = sin (theta * System.Math.PI / 180.0)
+    let rotate (x,y) = 
+      let dx = x - x0
+      let dy = y - y0
+      (x0 + cs*dx - sn*dy, y0 + sn*dx + cs*dy)
+    mapTail rotate c
+  static member ( |^) (c: Curve, theta: int) = c |^ (float theta) 
+  static member (-->) ((C((x0, y0), _) as c), (x1,y1)) = 
+    map (fun (x,y) -> (x - x0 + x1, (y - y0 + y1))) c
+  static member ( ><) (c, a) =  map (fun (x,y) -> (2.0*a - x, y)) c 
 
+let point       (x, y)            = C((x,y), [])
+let verticRefl  k                 = fun c -> map (fun (x,y) -> (x, 2.0*k - y)) c
+let boundingBox (C(p0, ps))       = 
+  let minmax ((minX, minY), (maxX, maxY)) (x,y) = ((min minX x, min minY y), (max maxX x, max maxY y))
+  List.fold minmax (p0, p0) ps 
+let width      c                  = match (boundingBox c) with
+                                      | ((minX, _), (maxX, _)) -> maxX - minX
+let height     c                  = match (boundingBox c) with
+                                      | ((_, minY), (_, maxY)) -> maxY - minY
+let toList     (C(p0, ps))        = p0::ps
--- chap7/Curve.fsi
+++ chap7/Curve.fsi
@@ -5,4 +5,15 @@ type Point = float*float
 [<Sealed>]
 type Curve =
   static member ( + ) : Curve * Curve -> Curve
-  static member ( * ) : float * Curve -> Curve
+  static member ( * ) : Curve * float -> Curve
+  static member ( |^) : Curve * float -> Curve
+  static member ( |^) : Curve * int   -> Curve
+  static member (-->) : Curve * Point -> Curve
+  static member ( ><) : Curve * float -> Curve
+  
+val point      : float * float -> Curve
+val verticRefl : float -> Curve -> Curve
+val boundingBox: Curve -> Point * Point
+val width      : Curve -> float 
+val height     : Curve -> float
+val toList     : Curve -> Point list
